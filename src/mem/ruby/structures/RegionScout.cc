ed6e3d0f56c70f65b2ad2b0823a063a3748205a0
Christopher Norris Chiminski <cchimins@qstruct.ecn.purdue.edu>
2021-04-24 17:50:19 -0400

    no errors on compilation -- moving forward to debugging/verifying

--- src/mem/ruby/structures/RegionScout.cc
+++ src/mem/ruby/structures/RegionScout.cc
@@ -1,35 +1,4 @@
-#ifndef __MEM_RUBY_STRUCTURES_REGIONSCOUT_HH__
-#define __MEM_RUBY_STRUCTURES_REGIONSCOUT_HH__
-
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include "mem/ruby/common/Address.hh"
-
-//ADDR is uint64_t
-
-///////////////////////////// NSRT /////////////////////////////
-class RegionScout
-{
-	public:
-		uint32_t NSRT_size = 64;
-		uint32_t MRU;
-		std::unordered_map<uint64_t, bool> nsrt_map; //unordered map to contain (region, valid);
-
-		bool verify_region(Addr address);						//verify if region is shared or not shared
-		bool isNSRTFull();													//check to see if NSRT is full
-		void append_NSRT(Addr address);							//region is unshared
-		void invalidate_NSRT(Addr address);					//region becomes shared				
-		void removeMRU_NSRT();											//NSRT is full, remove MRU
-
-		uint32_t CRH_size = 256;
-		uint32_t crh_vector[256]; 					//vector to contain (count);
-		void increment_CRH(Addr addr);								//increment on a block access
-		void decrement_CRH(Addr addr);								//decrement on a replacement of a block
-		bool check_CRH(Addr addr);
-
-
-};
+#include <mem/ruby/structures/RegionScout.hh>
 
 inline bool RegionScout::verify_region(Addr address)
 {
@@ -94,5 +63,3 @@ inline bool RegionScout::check_CRH(Addr address)
 
 }
 ///////////////////////////// CRH //////////////////////////////
-
-#endif //__MEM_RUBY_STRUCTURES_REGIONSCOUT_HH__
