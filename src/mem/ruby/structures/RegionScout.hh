1fdced950dea47cc36073affd48cca8927a09ac0
Christopher Norris Chiminski <cchimins@saguaro3.ecn.purdue.edu>
2021-04-27 17:12:38 -0400

    added jetty implementation, added more descriptive comments/debug statements

--- src/mem/ruby/structures/RegionScout.hh
+++ src/mem/ruby/structures/RegionScout.hh
@@ -6,8 +6,8 @@
 #include <vector>
 #include "mem/ruby/common/Address.hh"
 #include "mem/ruby/common/MachineID.hh"
-#define REGION_SIZE	(6)
-#define CRH_SIZE (1024*1024)
+#define REGION_SIZE	(8)
+#define CRH_SIZE (256)
 #define CRH_MASK (CRH_SIZE - 1)
 
 //ADDR is uint64_t
@@ -16,18 +16,21 @@
 class RegionScout
 {
 	public:
-		uint32_t NSRT_size = 64;
+		uint32_t NSRT_size = 1024*1024;
 		uint32_t MRU;
 		std::unordered_map<uint64_t, bool> nsrt_map; //unordered map to contain (region, valid);
 
 		bool verify_region(Addr address, MachineID machineID);						//verify if region is shared or not shared
 		bool isNSRTFull();													//check to see if NSRT is full
 		void append_NSRT(Addr address, MachineID machineID);							//region is unshared
-		void invalidate_NSRT(Addr address, MachineID machineID);					//region becomes shared				
-		void removeMRU_NSRT();											//NSRT is full, remove MRU
+		void invalidate_NSRT(Addr address, MachineID machineID, MachineID requestor);					//region becomes shared				
+		void removeMRU_NSRT(Addr address, MachineID machineID);											//NSRT is full, remove MRU
 
 		//uint32_t CRH_size = 256;
-		uint32_t crh_vector[CRH_SIZE]; 					//vector to contain (count);
+		uint32_t crh_vector_id0[CRH_SIZE]; 																//vector to contain (count);
+		uint32_t crh_vector_id1[CRH_SIZE]; 																//vector to contain (count);
+		uint32_t crh_vector_id2[CRH_SIZE]; 																//vector to contain (count);
+		uint32_t crh_vector_id3[CRH_SIZE]; 																//vector to contain (count);
 		void increment_CRH(Addr addr, MachineID machineID);								//increment on a block access
 		void decrement_CRH(Addr addr, MachineID machineID);								//decrement on a replacement of a block
 		bool check_CRH(Addr addr, MachineID machineID);
@@ -62,7 +65,7 @@ inline void RegionScout::append_NSRT(Addr address, MachineID machineID)
 	bool full = isNSRTFull();
 	if(full)
 	{
-		removeMRU_NSRT();
+		removeMRU_NSRT(address, machineID);
 	}
 	uint64_t region = address >> REGION_SIZE;
 	nsrt_map.insert({region, 1});
@@ -70,40 +73,75 @@ inline void RegionScout::append_NSRT(Addr address, MachineID machineID)
 	DPRINTF(RubySlicc, "NSRT appened: %X region is validated @ address %X, machine: %d\n",region, address, machineID);
 }
 
-inline void RegionScout::removeMRU_NSRT()
+inline void RegionScout::removeMRU_NSRT(Addr address, MachineID machineID)
 {
+	uint64_t region = address >> REGION_SIZE;
+	DPRINTF(RubySlicc, "NSRT MRU removed: %X region is invalidated @ address %X, machine: %d\n",region, address, machineID);
 	nsrt_map.erase(MRU);
 }
 
-inline void RegionScout::invalidate_NSRT(Addr address, MachineID machineID)
+inline void RegionScout::invalidate_NSRT(Addr address, MachineID machineID, MachineID requestor)
 {
 	uint64_t region = address >> REGION_SIZE;
 	nsrt_map.erase(region);
-	DPRINTF(RubySlicc, "NSRT invalidated: %X region is invalidated @ address %X, machine: %d\n",region, address, machineID);
+	DPRINTF(RubySlicc, "NSRT invalidated: %X region is invalidated @ address %X, machine: %d because of machine: %d\n",region, address, machineID, requestor);
 
 }
 
 inline void RegionScout::increment_CRH(Addr address, MachineID machineID)
 {
-	uint32_t index = (address >> REGION_SIZE) & CRH_MASK;
-	crh_vector[index] += 1;
-	DPRINTF(RubySlicc, "crh_vector[%d] after incr: %d @ address %X - machine %d\n",index, crh_vector[index], address, machineID);
+	uint32_t index0 = (address >> REGION_SIZE) & CRH_MASK;
+	uint32_t index1 = (address >> (REGION_SIZE + 8)) & CRH_MASK;
+	uint32_t index2 = (address >> (REGION_SIZE + 16)) & CRH_MASK;
+	uint32_t index3 = (address >> (REGION_SIZE + 24)) & CRH_MASK;
+	//if(!(crh_vector[index] <= 0)) {crh_vector[index] -= 1;}
+	crh_vector_id0[index0] += 1;
+	crh_vector_id1[index1] += 1;
+	crh_vector_id2[index2] += 1;
+	crh_vector_id3[index3] += 1;
+	DPRINTF(RubySlicc, "crh_vector_id0[%d] after incr: %d @ address %X - machine %d\n",index0, crh_vector_id0[index0], address, machineID);
+	DPRINTF(RubySlicc, "crh_vector_id1[%d] after incr: %d @ address %X - machine %d\n",index1, crh_vector_id1[index1], address, machineID);
+	DPRINTF(RubySlicc, "crh_vector_id2[%d] after incr: %d @ address %X - machine %d\n",index2, crh_vector_id2[index2], address, machineID);
+	DPRINTF(RubySlicc, "crh_vector_id3[%d] after incr: %d @ address %X - machine %d\n",index3, crh_vector_id3[index3], address, machineID);
 }
 
 inline void RegionScout::decrement_CRH(Addr address, MachineID machineID)
 {
-	uint32_t index = (address >> REGION_SIZE) & CRH_MASK;
-	//if(!(crh_vector[index] <= 0)) {crh_vector[index] -= 1;}
-	crh_vector[index] -= 1;
-	DPRINTF(RubySlicc, "crh_vector[%d] after dec: %d\n @ address %X - machine %d",index, crh_vector[index], address, machineID);
+	uint32_t index0 = (address >> REGION_SIZE) & CRH_MASK;
+	uint32_t index1 = (address >> (REGION_SIZE + 8)) & CRH_MASK;
+	uint32_t index2 = (address >> (REGION_SIZE + 16)) & CRH_MASK;
+	uint32_t index3 = (address >> (REGION_SIZE + 24)) & CRH_MASK;
+	crh_vector_id0[index0] -= 1;
+	crh_vector_id1[index1] -= 1;
+	crh_vector_id2[index2] -= 1;
+	crh_vector_id3[index3] -= 1;
+	DPRINTF(RubySlicc, "crh_vector_id0[%d] after dec: %d @ address %X - machine %d\n",index0, crh_vector_id0[index0], address, machineID);
+	DPRINTF(RubySlicc, "crh_vector_id1[%d] after dec: %d @ address %X - machine %d\n",index1, crh_vector_id1[index1], address, machineID);
+	DPRINTF(RubySlicc, "crh_vector_id2[%d] after dec: %d @ address %X - machine %d\n",index2, crh_vector_id2[index2], address, machineID);
+	DPRINTF(RubySlicc, "crh_vector_id3[%d] after dec: %d @ address %X - machine %d\n",index3, crh_vector_id3[index3], address, machineID);
+	
 }
 
 inline bool RegionScout::check_CRH(Addr address, MachineID machineID)
 {
-	uint32_t index = (address >> REGION_SIZE) & CRH_MASK;
-	DPRINTF(RubySlicc, "value of CRH[%d]: %d, @ %X\n - machine %d", index, crh_vector[index], address, machineID);
-	if(crh_vector[index] > 0) {return true;}
-	else {return false;}
+	uint32_t index0 = (address >> REGION_SIZE) & CRH_MASK;
+	uint32_t index1 = (address >> (REGION_SIZE + 8)) & CRH_MASK;
+	uint32_t index2 = (address >> (REGION_SIZE + 16)) & CRH_MASK;
+	uint32_t index3 = (address >> (REGION_SIZE + 24)) & CRH_MASK;
+	DPRINTF(RubySlicc, "value of crh_vector_id0[%d]: %d, @ %X - machine %d\n", index0, crh_vector_id0[index0], address, machineID);
+	DPRINTF(RubySlicc, "value of crh_vector_id1[%d]: %d, @ %X - machine %d\n", index1, crh_vector_id1[index1], address, machineID);
+	DPRINTF(RubySlicc, "value of crh_vector_id2[%d]: %d, @ %X - machine %d\n", index2, crh_vector_id2[index2], address, machineID);
+	DPRINTF(RubySlicc, "value of crh_vector_id3[%d]: %d, @ %X - machine %d\n", index3, crh_vector_id3[index3], address, machineID);
+	if((crh_vector_id0[index0] > 0) && (crh_vector_id1[index1] > 0) && (crh_vector_id2[index2] > 0) && (crh_vector_id3[index3] > 0)) 
+	{
+		DPRINTF(RubySlicc, "all indicies are greater than 0\n");
+		return true;
+	}
+	else 
+	{
+		DPRINTF(RubySlicc, "one or more indicies are eqaul to 0\n");
+		return false;
+}
 
 }
 ///////////////////////////// CRH //////////////////////////////
